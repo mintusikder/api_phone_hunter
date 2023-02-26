bca1551512b14a014c422a1077d6ea83b292ccef
mintusikder <mintu141617@gmail.com>
2023-02-26 19:34:40 +0600

    phone-container

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@ const displayPhons = phones =>{
     const phonesContainer =document.getElementById('phone-container')
     phonesContainer.textContent=''
     // display 20 phones only
+    phones=phones.slice(0,20)
+    // display no phones found
+    const noPhone =document.getElementById('no-found-message')
+    if(phones.length === 0){
+      noPhone.classList.remove('d-none')
+    }
+    else{
+      noPhone.classList.add('d-none')
+    }
+    //display all phones
     phones.forEach(phone => {
         const phoneDiv =document.createElement('div')
         phoneDiv.classList.add('col')
@@ -32,4 +42,4 @@ document.getElementById('btn-search').addEventListener('click',function(){
     loadPhones(searchText)
 
 })
-loadPhones()
+// loadPhones()
